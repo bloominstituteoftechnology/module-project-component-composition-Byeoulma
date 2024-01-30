e516f2aaee4d2ef516e12127afdc37ffba104070
Byeoulma <jazminelele@gmail.com>
2024-01-30 12:33:27 -0500

    WIP( starting first function set up after creating a wireframe on figma)

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+// creating a variable to store NASA url API
+const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY';
+
+console.log('App component has run');
 
 function App() {
+
+  // Create a function to grab photo
+  useEffect(() => {
+    function fetchPhoto() {
+      
+    }
+  })
+
+
+
   return (
     <p>
       Read through the instructions in the README.md file to build your NASA
